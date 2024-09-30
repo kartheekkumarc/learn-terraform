b2947a51d0c4988ce08ef235cb20ba7380031005
unknown <kartheek.c@gmail.com>
2024-09-30 14:09:52 +0530

    Add auto.tfvars file

--- terraform.tf
+++ terraform.tf
@@ -2,15 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-/*
+
   cloud {
-    organization = "organization-name"
+    organization = "thisissomeorg"
 
     workspaces {
       name = "learn-terraform"
     }
   }
-*/
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
